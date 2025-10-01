9b66654de376ef7ded493a612b6b89b11f00cff9
john.garcia.7e9 <john.garcia.7e9@itb.cat>
2025-10-01 09:29:30 +0200

    Nuevo contenido de archvivos

---
+++ TA02/ComprovarEdat.py
@@ -0,0 +1,15 @@
+"""
+John Steephen Garcia Duran
+01/10/2025
+ASIXc1B
+Ejemplo de apuntes
+"""
+# Programa que pide la edad y dice si eres mayor de edad o no:
+
+edad=int(input("Que edad tienes?:"))
+if edad>=18:
+    print("Felicidade', ya no ere' menol")
+else:
+    print("DIABLO MENOL")
+
+    "FIN DEL PROGRAMA"
--- TA02/helloWorld.py
+++ TA02/helloWorld.py
@@ -1,2 +1,4 @@
-print("Hola")
-
+#Prograna que ejecuta un saludo con el nombre introducido:
+nombre=input("Introduce tu nombre: ")
+print("hola", nombre)
+"Fin del programa"
